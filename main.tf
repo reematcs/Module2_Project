bd226d0185647a8452b91210a1f6a205a76501c7
Reem Mokhtar <reem.mokhtar@gmail.com>
2023-02-22 22:05:02 +0300

    modularizing

---
+++ main.tf
@@ -0,0 +1,11 @@
+# TODO: point to a specific version to Terraform/providers (aws) 
+# you can use the semantic version.terraform
+# terraform{
+# version = ">= 1.0" # semver 
+#       required_providers {
+#         aws = {
+#           source = "hashicorp/aws"
+#           version = "4.55.0"
+#         }
+#       }
+# }
---
+++ providers.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+	region     = "us-east-1"
+	access_key = "ASIA37M36QKRZCTYJTNK"
+	secret_key = "sso1mTd4j+6oBxZgfsDkTEqXvdgcAIwHEn4YhG+J" # TODO: REplace with file
+	token = "FwoGZXIvYXdzEFIaDFHdFduju3+BY4lE8SK3AWAsQ6jTmETTiftvBpxMbARKod4HEuYsLz5782bMxvoKg1ga/ZBK1Q7BL3ZCKRwn93xm6p37aJCDU75FHfEMmmETEDjzLqkxQSVKjkci6Fht9VY5S7xhZeeIxnEfphpfaGJLfxtK1nKB8+3TGCX8XCqrJn4phF0YiyjwryH3GGB3ciTxF9ky2e10fiKpxnJQ610RoAyZfE0Ox5s3aYk6apl/YgEEJ42IrPRQDsyjrJfONWTWLJ6RNCjSiNmfBjIt+QTaw3dRmQnAofx6qyIs+FBiaI0q9vG7fewqertqWvbp423qtJyiH3sP3VXO"
+}
