1ded85ddb4700e7a2aa0c478720537c8f3037acd
Reem Mokhtar <reem.mokhtar@gmail.com>
2023-02-26 16:48:22 +0300

    README + bucket acl "public-read", + inventory file creation to main.tf

--- bucket.tf
+++ bucket.tf
@@ -11,3 +11,24 @@ resource "aws_s3_bucket_acl" "ansible_provision" {
   bucket = aws_s3_bucket.b1.id
   acl    = "${var.acl_value}"
 }
+
+
+# resource "aws_s3_bucket_policy" "public_read_access" {
+#   bucket = aws_s3_bucket.b1.id
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+# 	  "Principal": "*",
+#       "Action": [ "s3:*" ],
+#       "Resource": [
+#         "${aws_s3_bucket.b1.arn}",
+#         "${aws_s3_bucket.b1.arn}/*"
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
--- variables.tf
+++ variables.tf
@@ -2,19 +2,19 @@ variable "ami_id" {
     default = "ami-00eeedc4036573771" 
 }
 variable "vpc_id"{
-    default="vpc-056d622c7a32e4729"
+    default="vpc-0723e281c6bdca95f"
 }
 variable "aws_terraform_keyname" {
     default = "aws_terraform"
 }
 variable "aws_access_key" {
-    default = "ASIA37M36QKR5ICDTSWB"
+    default = "ASIA37M36QKR5U52CFHL"
 }
 variable "aws_secret_key" {
-default = "hyoxDGRT68Ng77YKiRi6EHvAOLp9ur/PFGE30his"
+default = "wMK8a6ej//coPngpCZmGDoi/rl3NNbmkTMG4rhFD"
 }
 variable "aws_token" {
-  default = "FwoGZXIvYXdzEJ3//////////wEaDMciMATn78cZG/fgYyK3AbIjLOvhIQj8sQPfqUAjDY8dkVVAaWjvKQwsUfnh+bGUbuwOsotCoTug7vmYOklSU1vhQJs/b5n2AxrOPXbU+sSLJroDRgfVA76VpxUWrGLr93pW9I7QVen4NHKLirddwVw++5p8U5nuKM0EQQv/sgS122dHxuUOYCc0PhNLJjTNbiJAXfJWdNVLn8YOYfyb7VY4Df+icz6rrlGCau3dlOXL/jljbYW6rOaH6Di9Ms1D4+M+6aS0byiyvOmfBjItK8z3LX1EDH9S3F0M3sb1p/PAsXtOy+90Jpsc/jX3XW4S8uqDtPth/5BG5wf+"
+  default = "FwoGZXIvYXdzEK///////////wEaDLsTF9RK09PyPc1ikiK3AelyRFZ+xQQ/8uKUV+hA04C2UX38RCdrc+vXdaX65iQEoDuyz/XNLF177HACwq7Cxpl/81wU3G9BOsUBedUGztw4p8lb0Y0YgyhZNKBeJeMqdUoj7h4Xn3bxKjGWdCKg/6rrNAnPJfsLvhXB/O5vsFHpFR4DPqOJkZ48jxgl1+mGvPsJdy3B4xNFPhhlI1Uk28UsBFIrAszyYmkSZeaiGYkFF+zOJl4EL7kQ7B5V5An0pe/KUSxVHiivuu2fBjItBQ1be9xS/NJjVXF1dHPN2eHbW2zn/SrFg2z9q/kYO1ciUkwzBfK6uKAUxjda"
 }
 variable "region" {
     default = "region"
@@ -24,7 +24,7 @@ variable "bucket_name" {
     default = "project2-reem-ansibledir"
 }
 variable "acl_value" {
-    default = "public"
+    default = "public-read"
 }
 
 variable "ansible_zip" {
